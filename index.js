662b7cacb7d7a5b67d17e058e1816ffbc21bc75b
Travis Jones <toj817@gmail.com>
2024-02-02 19:46:29 -0800

    Added functions newImage and newItem

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(src, positionLeft, positionBottom) {
+    let image = document.createElement('img');
+    image.src = src;
+    image.style.position = 'fixed';
+    image.style.left = positionLeft;
+    image.style.bottom = positionBottom;
+
+    document.body.append(image);
+    return image;  // Return the created image
+}
+
+function newItem(image) {
+    image.addEventListener('dblclick', function() {
+        image.remove();
+    });
+}
+
+// Call list
+let greenCharacter = newImage('./assets/green-character.gif', '100px', '100px');
+let pineTree = newImage('./assets/pine-tree.png', '450px', '200px');
+let tree = newImage('./assets/tree.png', '200px', '300px');
+let pillar = newImage('./assets/pillar.png', '350px', '100px');
+let crate = newImage('./assets/crate.png', '150px', '200px');
+let well = newImage('./assets/well.png', '500px', '425px');
+
+let sword = newImage('./assets/sword.png', '500px', '405px');
+newItem(sword);
+let shield = newImage('assets/shield.png', '165', '185px');
+newItem(shield)
+let staff = newImage('assets/staff.png','600px','100px');
+newItem(staff)
+
